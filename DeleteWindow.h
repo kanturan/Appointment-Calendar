c3d4ed41db176c932cbf9453783df1223cc46e2d
Orville Reyes <reyes_orville@hotmail.com>
2016-02-09 22:28:09 +1300

    Create DeleteWindow.h

---
+++ DeleteWindow.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------
+
+#ifndef DeleteWindowH
+#define DeleteWindowH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm4 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Label1;
+	TLabel *Label2;
+	TLabel *Label7;
+	TButton *Button1;
+	TButton *Button2;
+	TEdit *Edit1;
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm4(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm4 *Form4;
+//---------------------------------------------------------------------------
+#endif
