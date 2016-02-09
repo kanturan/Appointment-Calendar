8df4f5222356754fb9404dfa7addb62b8dde509a
Orville Reyes <reyes_orville@hotmail.com>
2016-02-09 22:27:35 +1300

    Create EditWindow.h

---
+++ EditWindow.h
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+
+#ifndef EditWindowH
+#define EditWindowH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm3 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Label1;
+	TLabel *Label2;
+	TLabel *Label3;
+	TLabel *Label4;
+	TEdit *Edit1;
+	TEdit *Edit2;
+	TButton *Button1;
+	TButton *Button2;
+	TLabel *Label7;
+	TLabel *Label10;
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm3(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm3 *Form3;
+//---------------------------------------------------------------------------
+#endif
